9191f98e32442bc1d0e9518e2ebab47bfec2038e
curoky <cccuroky@gmail.com>
2026-08-09 22:55:56 +0800

    feat(uninstall.sh): add script to clean up trae and trae-cn user data

---
+++ uninstall.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+rm -f $HOME/.trae/rules
+rm -f $HOME/.trae-cn/rules
+rm -rf $HOME/.trae/user_rules
+rm -rf $HOME/.trae-cn/user_rules
+rm -rf $HOME/.trae/skills
+rm -rf $HOME/.trae-cn/skills
